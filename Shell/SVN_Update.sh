d4e721ad88c1f694a8ad2802965ee67956b4abbb
johnsonabc <34850814+johnsonabc@users.noreply.github.com>
2019-06-07 03:14:31 +0530

    usage description updated

--- Shell/SVN_Update.sh
+++ Shell/SVN_Update.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/sh
+#Usage of this script is 
+#sh SVN_Update.sh <input_file_name>
+#input filw should be in the format <component><space><Revision>
+#So that thhis script can update the component to revisions mentioed in the input
+#for generating the input use the script "SVNRevision.sh"
+
 filename=$1
 while read line; do
   # reading each line
